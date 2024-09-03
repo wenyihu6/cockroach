07dfa9569740de1541a3447fdfb1faaaeb6c0518
Wenyi Hu <wenyi@cockroachlabs.com>
2024-09-03 09:06:44 -0400

    kvserver/rangefeed: add capacity to node level buffered sender
    This patch adds capacity to node level buffered sender which will shut down all
    registrations if the node level buffer had overflowed.
    
    Part of: https://github.com/cockroachdb/cockroach/issues/129813
    Release note: none
    
    TODO: add a larger test at the kvclient side to make sure error returned here is treated as a restart signal

--- pkg/kv/kvserver/rangefeed/buffered_sender.go
+++ pkg/kv/kvserver/rangefeed/buffered_sender.go
@@ -7,16 +7,21 @@ package rangefeed
 
 import (
 	"context"
+	"math"
 	"sync"
 	"time"
 
 	"github.com/cockroachdb/cockroach/pkg/kv/kvpb"
+	"github.com/cockroachdb/cockroach/pkg/util/envutil"
 	"github.com/cockroachdb/cockroach/pkg/util/log"
 	"github.com/cockroachdb/cockroach/pkg/util/retry"
 	"github.com/cockroachdb/cockroach/pkg/util/stop"
 	"github.com/cockroachdb/cockroach/pkg/util/syncutil"
 	"github.com/cockroachdb/errors"
 )
+
+var bufferedSenderCapacity = envutil.EnvOrDefaultInt64(
+	"COCKROACH_BUFFERED_STREAM_CAPACITY_PER_STREAM", math.MaxInt64)
 
 type sharedMuxEvent struct {
 	event *kvpb.MuxRangeFeedEvent
@@ -90,8 +95,10 @@ type BufferedSender struct {
 
 	queueMu struct {
 		syncutil.Mutex
-		stopped bool
-		buffer  *eventQueue
+		stopped  bool
+		capacity int64
+		buffer   *eventQueue
+		overflow bool
 	}
 
 	// Unblocking channel to notify the BufferedSender.run goroutine that there
@@ -107,6 +114,7 @@ func NewBufferedSender(
 		metrics: metrics,
 	}
 	bs.queueMu.buffer = newEventQueue()
+	bs.queueMu.capacity = bufferedSenderCapacity
 	bs.notifyDataC = make(chan struct{}, 1)
 	return bs
 }
@@ -125,6 +133,14 @@ func (bs *BufferedSender) SendBuffered(
 	if bs.queueMu.stopped {
 		log.Errorf(context.Background(), "stream sender is stopped")
 		return errors.New("stream sender is stopped")
+	}
+	if bs.queueMu.overflow {
+		log.Error(context.Background(), "buffer capacity exceeded")
+		return newRetryErrBufferCapacityExceeded()
+	}
+	if bs.queueMu.buffer.Len() >= bs.queueMu.capacity {
+		bs.queueMu.overflow = true
+		return newRetryErrBufferCapacityExceeded()
 	}
 	alloc.Use(context.Background())
 	bs.queueMu.buffer.pushBack(sharedMuxEvent{ev, alloc})
@@ -232,7 +248,7 @@ func (bs *BufferedSender) run(ctx context.Context, stopper *stop.Stopper) error 
 			return nil
 		case <-bs.notifyDataC:
 			for {
-				e, success := bs.popFront()
+				e, success, overflowed, remains := bs.popFront()
 				if success {
 					err := bs.sender.Send(e.event)
 					e.alloc.Release(ctx)
@@ -247,6 +263,9 @@ func (bs *BufferedSender) run(ctx context.Context, stopper *stop.Stopper) error 
 					if err != nil {
 						return err
 					}
+					if overflowed && remains == int64(0) {
+						return newRetryErrBufferCapacityExceeded()
+					}
 				} else {
 					break
 				}
@@ -255,11 +274,16 @@ func (bs *BufferedSender) run(ctx context.Context, stopper *stop.Stopper) error 
 	}
 }
 
-func (bs *BufferedSender) popFront() (e sharedMuxEvent, success bool) {
+func (bs *BufferedSender) popFront() (
+	e sharedMuxEvent,
+	success bool,
+	overflowed bool,
+	remains int64,
+) {
 	bs.queueMu.Lock()
 	defer bs.queueMu.Unlock()
 	event, ok := bs.queueMu.buffer.popFront()
-	return event, ok
+	return event, ok, bs.queueMu.overflow, bs.queueMu.buffer.Len()
 }
 
 func (bs *BufferedSender) Start(ctx context.Context, stopper *stop.Stopper) error {
